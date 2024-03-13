ce78ac8897e70747b01b7a1a84aa633b2da5d380
sahuagin0 <arlo@picante.com>
2024-03-13 19:46:50 -0400

    downloads files named with message text

--- slack_socketmode2.py
+++ slack_socketmode2.py
@@ -2,15 +2,17 @@ import os
 import logging
 import requests
 import pathlib
+import slack
 from urllib.parse import urlparse
+from slack_sdk import WebClient
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 logging.basicConfig(level=logging.DEBUG)
 
-with open(".slack_apptoken", "r") as f:
+with open("./slack_apptoken", "r") as f:
     auth_app_token=f.read() 
 
-with open(".slack_token", "r") as f:
+with open("./slack_token", "r") as f:
     auth_token=f.read() 
 
 app = App(token=auth_app_token)
@@ -26,7 +28,23 @@ def handle_mention(event, say):
         u = urlparse(f["url_private"])
         p = pathlib.Path(u.path)
         r=requests.get(f["url_private"], headers={"Authorization": f"Bearer {auth_token}"})
-        with open(p.name, 'wb') as file:
+        with open(event["text"] + "." + f["filetype"], 'ab') as file:
+           file.write(r.content)
+           
+#35-48: download all past files not already downloaded from a conversation
+client = WebClient(auth_token)
+response = client.conversations_history(
+   channel=""
+)
+assert response["ok"]
+messages = response["messages"]
+
+for m in messages:
+   text = m["text"]
+   if "files" in m:
+      for f in m["files"]:
+        r=requests.get(f["url_private"], headers={"Authorization": f"Bearer {auth_token}"})
+        with open(text+"."+f["filetype"], "ab") as file:
            file.write(r.content)
 
 
